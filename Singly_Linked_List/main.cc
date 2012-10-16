20c3d80986afecac3dde93a458f3e70be308339e
Dparker1990 <parkerderek86@gmail.com>
2012-10-15 22:27:23 -0500

    Cleaning memory leaks better -- like a sir

--- Singly_Linked_List/main.cc
+++ Singly_Linked_List/main.cc
@@ -2,11 +2,9 @@
 #include "Singly_Linked_List.h"
 
 int main(int argvc, const char **argv){
-    SinglyLinkedList *list = new SinglyLinkedList();
+    SinglyLinkedList list;
     for (int i = 0; i < 10; i++){
-        list->append(i);
+        list.append(i);
     }
-    std::cout << list->find(7) << std::endl;
-    
-    delete list;
+    std::cout << list.find(7) << std::endl;
 }
