af4358ff79309c75ed54d8d225aa1513e78b7693
Derek Parker <parkerderek86@gmail.com>
2012-12-13 23:54:59 -0600

    Found optimal route with correct answer

--- 18_and_67/triangle-route.rb
+++ 18_and_67/triangle-route.rb
@@ -1,19 +1,22 @@
 def find_route(t)
-  total = 0
-  last_max = 1
-  t.each_pair do |key, val|
-    min = last_max - 1 < 0 ? last_max : last_max - 1
-    v, index = val[(min)..(last_max+1)].each_with_index.max
-    last_max = index
-    total += v
+  i = (t.length - 2)
+  while i >= 0
+    j = 0
+    while j < t[i].length
+      t[i][j] += t[i+1][j] < t[i+1][j+1] ? t[i+1][j+1] : t[i+1][j]
+      j += 1
+    end
+    i -= 1
+    t.pop()
   end
-  total
+
+  t[0][0]
 end
 
 def main
   i, j = 0, 0
-  t = {}
-  t2 = {}
+  t = []
+  t2 = []
   
   File.open("num.txt", "r").each_line do |line|
     t[j] = line.split(" ").map { |x| x.to_i }
@@ -26,6 +29,7 @@ def main
   end
 
   puts find_route(t)
+  total = 0
   puts find_route(t2)
 end
 
